f0188861e7fdbd6fec340abfba298aeb3939b69a
Kir <kozinkirill85@gmail.com>
2026-03-30 22:46:15 +0300

    feat: добавлены страница товара и базовый шаблон

--- skystore/urls.py
+++ skystore/urls.py
@@ -2,10 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('catalog.urls')),
+    path('', views.home, name='home'),
+    path('contacts/', views.contacts, name='contacts'),
+    path('product/<int:pk>/', views.product_detail, name='product_detail'),
 ]
 
 # Добавление маршрутов для медиафайлов в режиме разработки

d479003c00f152b92c032d6f40da577cf9ea81ab
Kir <kozinkirill85@gmail.com>
2026-03-30 11:20:32 +0300

    feat: добавлены категории и продукты через админку

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """Админка для модели Category"""
+    list_display = ('id', 'name')
+    search_fields = ('name',)
+    list_filter = ('name',)
+
+    def get_readonly_fields(self, request, obj=None):
+        """Поля, которые нельзя редактировать"""
+        return ('created_at', 'updated_at') if obj else ()
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    """Админка для модели Product"""
+    list_display = ('id', 'name', 'price', 'category')
+    list_filter = ('category',)
+    search_fields = ('name', 'description')
+
+    def get_readonly_fields(self, request, obj=None):
+        """Поля, которые нельзя редактировать"""
+        return ('created_at', 'updated_at') if obj else ()
